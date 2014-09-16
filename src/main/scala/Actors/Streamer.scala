73e898c0052b9ef03993b232bc242d356540d3a8
tbrown1979 <tbrown@uni.edu>
2014-09-16 14:41:32 -0500

    Removing some unneeded code from the Streamer

--- src/main/scala/Actors/Streamer.scala
+++ src/main/scala/Actors/Streamer.scala
@@ -19,19 +19,9 @@ class Streamer(client: ActorRef, count: Int) extends Actor with ActorLogging {
     context.system.eventStream.subscribe(context.self, classOf[TweetJson])
   }
 
-  // we use the successful sending of a chunk as trigger for scheduling the next chunk
-  client ! ChunkedResponseStart(HttpResponse(entity = " " * 2048))//.withAck(Ok(count))
+  client ! ChunkedResponseStart(HttpResponse(entity = ""))
 
   def receive = {
-    case Ok(0) =>
-      log.info("Finalizing response stream ...")
-      client ! MessageChunk("\nStopped...")
-      client ! ChunkedMessageEnd
-      context.stop(self)
-
-    case Ok(remaining) =>
-      log.info("Sending response chunk ...")
-
     case TweetJson(json) =>
       log.info("Sending Tweet json!")
       client ! MessageChunk(json)
@@ -40,7 +30,4 @@ class Streamer(client: ActorRef, count: Int) extends Actor with ActorLogging {
       log.info("Canceling response stream due to {} ...", x)
       context.stop(self)
   }
-
-  // simple case class whose instances we use as send confirmation message for streaming chunks
-  case class Ok(remaining: Int)
 }
